c9d5fabf8a06af716a9a227dd2806763680ea261
Dessi <dessix@dessix.net>
2014-11-19 23:22:31 -0500

    Added command line example which currently does nothing.

--- MPM/Program.cs
+++ MPM/Program.cs
@@ -1,12 +1,49 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using PowerArgs;
 
 namespace MPM {
-	class Program {
-		static void Main(string[] args) {
+	[ArgExceptionBehavior(ArgExceptionPolicy.StandardExceptionHandling)]
+	public class LaunchArgs {
+		[ArgDescription("Prints all messages to stdout")]
+		[ArgShortcut("--verbose"), ArgShortcut("-v")]
+		[ArgEnforceCase]
+		public bool Verbose { get; set; }
+
+		[HelpHook]
+		[ArgShortcut("--help"), ArgShortcut("-h")]
+		[ArgEnforceCase]
+		public bool Help { get; set; }
+
+		[ArgActionMethod]
+		[ArgShortcut("--uc")]
+		public void UpdateCauldron(UpdateCauldronArgs args) {
+			Console.WriteLine("UpdateCauldron called, file was " + args.CauldronFile);
+		}
+	}
+	public class UpdateCauldronArgs {
+		[ArgRequired(PromptIfMissing = true)]
+		[ArgDescription("The Cauldron installer file to update")]
+		[ArgPosition(1)]
+		[ArgExistingFile]
+		public string CauldronFile { get; set; }
+	}
+	public class Program {
+		public static void Main(string[] args) {
+			ArgAction<LaunchArgs> parsed;
+			try {
+				parsed = Args.InvokeAction<LaunchArgs>(args);
+			} catch (ArgException ex) {
+				Console.WriteLine(ex.Message);
+				Console.WriteLine(ArgUsage.GenerateUsageFromTemplate<LaunchArgs>());
+				return;
+			}
+			if (parsed.Args == null) {
+				return;
+			}
 		}
 	}
 }
