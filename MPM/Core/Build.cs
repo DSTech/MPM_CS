ecb12e9ec1f88198a7332eae406f8187b864b1f7
Dessi <Dessix@Dessix.net>
2015-02-23 06:48:16 -0500

    Removed PackageName from individual Build entries.

--- MPM/Core/Build.cs
+++ MPM/Core/Build.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 
 namespace MPM.Core {
 	public class Build {
-		public string PackageName { get; set; }
 		public VersionIdentifier Version { get; set; }
 		public string GivenVersion { get; set; }
 		public InterfaceProvision[] InterfaceProvisions { get; set; }
