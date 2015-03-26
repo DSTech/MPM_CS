0a0398602f14f7fbaa419032c6d26af424056c73
Dessi <Dessix@Dessix.net>
2015-03-26 15:28:36 -0400

    Added IPackageRepository and IPackageRepositoryCache, with associated documentation.

---
+++ MPM/Data/IPackageRepository.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Collections.Generic;
+using MPM.Net.DTO;
+using semver.tools;
+
+namespace MPM.Data {
+	/// <summary>
+	/// Provides a package repository interface to allow the loading of package information.
+	/// </summary>
+	public interface IPackageRepository {
+		/// <summary>
+		/// Fetches a list of all packages, but is not required to return any particular data (eg. authors) for each package.
+		/// Should be used to find packages for which further information may be looked up via
+		/// <see cref="FetchPackage(string)"/> or <see cref="FetchBuild(string, SemanticVersion)"/>.
+		/// </summary>
+		/// <returns>Multiple <see cref="Package"/> instances containing, at minimum, the name of the package.</returns>
+		IEnumerable<Package> FetchPackageList();
+		/// <summary>
+		/// Similar to <see cref="FetchPackageList"/>, fetches only packages which have changed since the specified <paramref name="updatedAfter"/> time.
+		/// May contain more packages than strictly those updated after the specified time, depending on the repository's server-side implementation.
+		/// </summary>
+		/// <param name="updatedAfter"></param>
+		/// <returns>Multiple <see cref="Package"/> instances containing, at minimum, the name of the package.</returns>
+		IEnumerable<Package> FetchPackageList(DateTime updatedAfter);
+		Package FetchPackage(String packageName);
+		/// <summary>
+		/// Looks up a particular build of a package.
+		/// </summary>
+		/// <param name="packageName"></param>
+		/// <param name="version"></param>
+		/// <returns>A <see cref="Build"/> instance matching the specified <paramref name="packageName"/> and <paramref name="version"/>. Null when no package of the specified name was found.</returns>
+		Build FetchBuild(String packageName, SemanticVersion version);
+		/// <summary>
+		/// Looks up a package for any versions matching a specifier.
+		/// </summary>
+		/// <param name="packageName">Name of the package to look up</param>
+		/// <param name="versionSpec">The version specification to check against before returning a build</param>
+		/// <returns>A <see cref="Package"/> instance containing only builds which comply with the <paramref name="versionSpec"/> given. Null when no package of the specified name was found.</returns>
+		Package FetchBuilds(String packageName, VersionSpec versionSpec);
+	}
+}
---
+++ MPM/Data/IPackageRepositoryCache.cs
@@ -0,0 +1,13 @@
+namespace MPM.Data {
+	/// <summary>
+	/// Provides a wrapper around <see cref="IPackageRepository"/> with which the calls may be cached to reduce repository load and increase responsiveness.
+	/// </summary>
+	public interface IPackageRepositoryCache : IPackageRepository {
+		/// <summary>
+		/// Should be called at least once before usage, unless the particular implementation has loaded information from another source.
+		/// Provides a means for the cache to retreive any changed package information from the repository.
+		/// It should be assumed that the longer the duration since the last call to this function is, the less accurate the cache may be to the current state of the repository.
+		/// </summary>
+		void Sync();
+	}
+}
