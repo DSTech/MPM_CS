3c7310a8652eca9c13bf82252325ee62dcd2eefb
Dessi <Dessix@Dessix.net>
2015-05-02 12:28:21 -0400

    Added package Platform.VirtualFileSystem, in hopes that it will be more useful than IFileSystem has been if it is used to rebuild it. Fixed build.

--- MPM/Core/FileSystem/FileSystem.cs
+++ MPM/Core/FileSystem/FileSystem.cs
@@ -1,4 +1,9 @@
+using System;
+
 namespace MPM.Core.FileSystem {
 	public class FileSystem : IFileSystem {
+		public void Apply(IFileTreeDelta delta) {
+			throw new NotImplementedException();
+		}
 	}
 }
