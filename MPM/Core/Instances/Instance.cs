5c2e09802f5d24af3c8cb3fc1ec48f5822a66fe1
Dessi <Dessix@Dessix.net>
2015-06-18 19:47:36 -0400

    Implemented Instance.Configuration.

--- MPM/Core/Instances/Instance.cs
+++ MPM/Core/Instances/Instance.cs
@@ -5,25 +5,58 @@ using System.Text;
 using System.Threading.Tasks;
 using MPM.Core.Dependency;
 using MPM.Core.Instances.Installation;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
 using Platform.VirtualFileSystem;
 using Platform.VirtualFileSystem.Providers.Local;
 
 namespace MPM.Core.Instances {
 	public class Instance {
 		public String Name { get; set; }
-        public String Location { get; set; }
+		public String Location { get; set; }
 		public Type LauncherType { get; set; } = typeof(MinecraftLauncher);//TODO: Change to a default (ScriptLauncher / ShellLauncher?), or auto-identify launch method
-		
+
 		public IFileSystem GetFileSystem() {
-            return FileSystemManager.Default.ResolveDirectory(Location).CreateView();
+			return FileSystemManager.Default.ResolveDirectory(Location).CreateView();
 		}
 
+		const string MpmDirectory = ".mpm";
+		const string ConfigurationName = "configuration.json";
+		readonly JsonSerializerSettings JsonSettings = new JsonSerializerSettings {
+			TypeNameHandling = TypeNameHandling.Auto,
+			Formatting = Formatting.Indented,
+			ObjectCreationHandling = ObjectCreationHandling.Replace,
+		};
 		public InstanceConfiguration @Configuration {
 			get {
-				throw new NotImplementedException();
+				using (var fileSystem = GetFileSystem()) {
+					var mpmDir = fileSystem.ResolveDirectory(MpmDirectory);
+					if (!mpmDir.Exists) {
+						return InstanceConfiguration.Empty;
+					}
+					var configFile = mpmDir.ResolveFile(ConfigurationName);
+					if (!configFile.Exists) {
+						return InstanceConfiguration.Empty;
+					}
+                    var configReader = configFile.GetContent().GetReader();
+					using (var jsonReader = new JsonTextReader(configReader)) {
+						return JsonSerializer.Create(JsonSettings).Deserialize<InstanceConfiguration>(jsonReader);
+					}
+				}
 			}
 			set {
-				throw new NotImplementedException();
+				using (var fileSystem = GetFileSystem()) {
+					var mpmDir = fileSystem.ResolveDirectory(MpmDirectory);
+					if (!mpmDir.Exists) {
+						mpmDir.Create(true);
+					}
+					var configFile = fileSystem.ResolveFile(ConfigurationName);
+					configFile.ResolveDirectory(".").Create();
+					var configWriter = configFile.GetContent().GetWriter();
+					using (var jsonWriter = new JsonTextWriter(configWriter)) {
+						JsonSerializer.Create(JsonSettings).Serialize(jsonWriter, value, typeof(InstanceConfiguration));
+					}
+				}
 			}
 		}
 
