de21325dc568515d1841659df83010294b203303
Dessi <Dessix@Dessix.net>
2015-06-19 16:57:22 -0400

    Began creation of IFileDeclaration.Parse.

--- MPM/Core/Instances/Info/ScriptFileDeclaration.cs
+++ MPM/Core/Instances/Info/ScriptFileDeclaration.cs
@@ -1,5 +1,6 @@
 using System;
 using MPM.Core.Instances.Installation.Scripts;
+using semver.tools;
 
 namespace MPM.Core.Instances.Info {
 	public class ScriptFileDeclaration {
@@ -9,9 +10,32 @@ namespace MPM.Core.Instances.Info {
 		public String Hash { get; set; }
 		public String Target { get; set; }
 		public String[] Targets { get; set; }
-		public IFileDeclaration Parse() {
+		public IFileDeclaration Parse(String packageName, SemanticVersion packageVersion) {
 			//Determine what type of file declaration to create depending upon class members
-			throw new NotImplementedException();
+			if (Source == null && Type != null) {
+				var decl = new SourcelessFileDeclaration() {
+					Description = Description,
+					PackageName = packageName,
+					PackageVersion = packageVersion,
+					Targets = (Target != null ? new[] { Target } : Targets ?? new String[0]),
+				};
+				switch (Type) {
+					case "configuration":
+						decl.Type = SourcelessType.Configuration;
+						return decl;
+					case "cache":
+						decl.Type = SourcelessType.Cache;
+						return decl;
+					default:
+						throw new NotSupportedException($"File declaration type {Type} not supported");
+				}
+			}
+			//TODO: Add support for other declaration types
+			{
+				var e = new NotSupportedException($"File declaration not supported");
+				e.Data.Add(nameof(ScriptFileDeclaration), this);
+				throw e;
+			}
 		}
 	}
 }
