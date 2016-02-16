613c50ad10bac2b76c63f7a8b7b0d12d7e25ca3c
Zoey <Dessix@Dessix.net>
2016-02-15 23:58:59 -0500

    Changed asset parsing to hold URIs for asset paths.

--- MPM/Net/Protocols/Minecraft/ProtocolTypes/AssetCollection.cs
+++ MPM/Net/Protocols/Minecraft/ProtocolTypes/AssetCollection.cs
@@ -1,9 +1,10 @@
+using System;
 using System.Collections.Generic;
 using Newtonsoft.Json;
 
 namespace MPM.Net.Protocols.Minecraft.ProtocolTypes {
     public class AssetCollection {
         [JsonProperty("objects", Required = Required.Always)]
-        public Dictionary<string, Asset> Assets { get; set; }
+        public Dictionary<Uri, Asset> Assets { get; set; }
     }
 }
